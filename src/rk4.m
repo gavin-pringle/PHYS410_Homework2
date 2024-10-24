0d01e9fea93a77046066ad016808c411cc335952
Gavin Pringle <gavinpringle00@gmail.com>
2024-10-23 22:22:43 -0700

    Completed P1 and P2 functions, need to test

--- src/rk4.m
+++ src/rk4.m
@@ -1,15 +1,31 @@
-%% Problem 2 - Runge-Kutta 
+%% Problem 2 - Runge-Kutta System of ODEs Integrator
 
 % Inputs
-%       fcn: Function handle for right hand sides of ODEs (returns
-%       length-n column vector)
-%       tspan: Vector of output times (length nout).
-%       y0: Initial values (length-n column vector).
+%       fcn:    Function handle for right hand sides of ODEs (returns
+%               length-n column vector)
+%       tspan:  Vector of output times (length nout).
+%       y0:     Initial values (length-n column vector).
 %
 % Outputs
-%       tout: Vector of output times.
-%       yout: Output values (nout x n array. The ith column of yout
-%       contains the nout values of the ith dependent variable).
+%       tout:   Vector of output times.
+%       yout:   Output values (nout x n array. The ith column of yout
+%               contains the nout values of the ith dependent variable).
 function [tout yout] = rk4(fcn, tspan, y0)
-    tout = 0;
-    yout = 0;
+    % Number of equations in ODE system
+    n = max(size(y0));
+    % Number of time-steps
+    nout = max(size(tspan));
+    % Step size
+    dt = tspan(2) - tspan(1);
+
+    % Initialize array for output values 
+    yout = zeros(nout, n);
+    yout(1,:) = y0.';
+
+    % Integrate ODE 
+    for i = 2:nout
+        yout(i,:) = rk4step(fcn, tspan(i-1), dt, yout(i-1,:).').';
+    end 
+
+    % Generate array of output values 
+    tout = tspan;
--- src/rk4step.m
+++ src/rk4step.m
@@ -1,13 +1,19 @@
 %% Problem 1 - Single Fourth Order Runge-Kutta Step
 
 % Inputs
-%       fcn: Function handle for right hand sides of ODEs (returns
-%       length-n column vector).
-%       t0: Initial value of independent variable.
-%       dt: Time step.
-%       y0: Initial values (length-n column vector).
+%       fcn:    Function handle for right hand sides of ODEs (returns
+%               length-n column vector).
+%       t0:     Initial value of independent variable.
+%       dt:     Time step.
+%       y0:     Initial values (length-n column vector).
 %
 % Output
-%       yout: Final values (length-n column vector)
+%       yout:   Final values (length-n column vector)
 function yout = rk4step(fcn, t0, dt, y0)
-    yout = 0;
+    % Compute terms in RK step
+    f0 = fcn(t0, y0);
+    f1 = fcn(t0 + dt/2, y0 + (dt/2)*f0);
+    f2 = fcn(t0 + dt/2, y0 + (dt/2)*f1);
+    f3 = fcn(t0 + dt, y0 + dt*f2);
+    % Add terms to compute full RK step 
+    yout = y0 + (dt/6)*(f0 + 2*f1 + 2*f2 + f3);
