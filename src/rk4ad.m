b73bf568e62c3ee5cb2fa0eb3e3672baa2ef8c4f
Gavin Pringle <gavinpringle00@gmail.com>
2024-10-25 12:49:46 -0700

    rk4ad in progress

--- src/rk4ad.m
+++ src/rk4ad.m
@@ -19,7 +19,6 @@ function [tout yout] = rk4ad(fcn, tspan, reltol, y0)
     n = max(size(y0));
     % Number of time-steps
     nout = max(size(tspan));
-
     % Lower bound on step size
     floor = 1.0e-4;
 
@@ -29,11 +28,32 @@ function [tout yout] = rk4ad(fcn, tspan, reltol, y0)
 
     % Integrate ODE 
     for i = 2:nout
-        yout(i,:) = rk4step(fcn, tspan(i-1), dt, yout(i-1,:).').';
+        % Compute coarse rk4step arguments  
+        t0 = tspan(i-1);
+        y0 = yout(i-1,:).';
+        dt = tspan(i) - tspan(i-1);
+
+        % Compute fine and coarse appromations for y(t0 + dt)
+        yc = rk4step(fcn, t0, dt, y0);
+        if dt/2 < floor
+            % If fine step is lower than floor, cannot narrow down any further
+            yout(i,:) = yc.';
+            continue;
+        end 
+        yhalf = rk4step(fcn, t0, dt/2, y0);
+        yf = rk4step(fcn, t0 + dt/2, dt/2, yhalf);
+
+        % Check if error meets relative tolerance parameter
+        if abs((yc - yf)/yf) < reltol
+            yout(i,:) = yf.';
+            continue;
+        else 
+            % Iteratively halve dt until reltol is met or floor is reached
+        end 
     end 
 end
 
 % NOTES:
 % Consider that tspan points might not be equidistant
-% tout may have more entries than tspan 
+% RELATIVE tolerance - divide by y
 % 
